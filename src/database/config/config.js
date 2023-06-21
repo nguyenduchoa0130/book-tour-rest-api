ee5365c6f82fa9957e4be90f28861ce77e54258f
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-06-21 20:22:16 +0700

    convert to string

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DEV_USERNAME,
-    password: process.env.DEV_PASSWORD,
-    database: process.env.DEV_DB,
-    host: process.env.DEV_HOST,
+    username: process.env.DEV_USERNAME.toString(),
+    password: process.env.DEV_PASSWORD.toString(),
+    database: process.env.DEV_DB.toString(),
+    host: process.env.DEV_HOST.toString(),
     dialect: 'postgres',
   },
   production: {
-    username: process.env.PROD_USERNAME,
-    password: process.env.PROD_PASSWORD,
-    database: process.env.PROD_DB,
-    host: process.env.PROD_HOST,
+    username: process.env.PROD_USERNAME.toString(),
+    password: process.env.PROD_PASSWORD.toString(),
+    database: process.env.PROD_DB.toString(),
+    host: process.env.PROD_HOST.toString(),
     dialect: 'postgres',
   },
 };
